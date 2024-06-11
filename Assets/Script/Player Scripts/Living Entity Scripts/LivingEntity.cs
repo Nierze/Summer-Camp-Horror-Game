3633d1fe9dc4e2e0481df431b8639f206e8ac0c1
Ren <melchorbulanon2@gmail.com>
2024-06-11 15:55:22 +0800

    Polished Insanity meter
    - Polished insanity meter
    - Sorted scripts folder
    - renamed some script

---
+++ Assets/Script/Player Scripts/Living Entity Scripts/LivingEntity.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour
+{
+    public float HP = 100;
+    public float DEF = 0;
+    public float MDEF = 0;
+    public float ATKmul = 1f;
+    public float DMGReduction = 0;
+
+
+}
---
+++ Assets/Script/Player Scripts/Living Entity Scripts/Player Scripts/InsanityMeterHandler.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InsanityMeterHandler : MonoBehaviour
+{
+    [SerializeField]
+    private float maxInsanityMeter = 100f;
+    
+    [SerializeField]
+    private float currrentInsanityMeter = 0f;
+    
+    [SerializeField]
+    private float currentInsanityBuildUp = 1f;
+    
+    [SerializeField]
+    private float currentInsanityBuildUpMultiplier = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        currrentInsanityMeter = currrentInsanityMeter + CalculateInsanity();
+        if (currentInsanityBuildUp <= 0 ) {
+            currrentInsanityMeter -= 0.01f;
+        }
+        if (currrentInsanityMeter <= 0) {
+            currrentInsanityMeter = 0;
+        }
+        Debug.Log("Insanity: " + Math.Round(currrentInsanityMeter,2));
+        // Debug.Log("Rand" + UnityEngine.Random.Range(0f, currentInsanityBuildUp));
+    }
+
+    float CalculateInsanity()
+    {
+        float randomValue1 = UnityEngine.Random.Range(0f, currentInsanityBuildUp);
+        float randomValue2 = UnityEngine.Random.Range(0f, currentInsanityBuildUp);
+
+        float insanityValue = randomValue1 + (randomValue2*-1);
+
+        return insanityValue * currentInsanityBuildUpMultiplier;
+    }
+}
