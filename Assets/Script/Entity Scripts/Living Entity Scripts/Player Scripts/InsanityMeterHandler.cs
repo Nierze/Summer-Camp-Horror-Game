7ca2ec569a2255048f81956d91629051dd157144
Ren <melchorbulanon2@gmail.com>
2024-06-14 11:09:07 +0800

    Choi major-implementation
    - Added insanity bar

--- Assets/Script/Entity Scripts/Living Entity Scripts/Player Scripts/InsanityMeterHandler.cs
+++ Assets/Script/Entity Scripts/Living Entity Scripts/Player Scripts/InsanityMeterHandler.cs
@@ -17,18 +17,44 @@ public class InsanityMeterHandler : MonoBehaviour
     [SerializeField]
     private float currentInsanityBuildUpMultiplier = 1f;
 
+    public RectTransform insanityMeter;
+
+
+    float percentUnit;
+    float insanityPercentUnit;
+
     // Update is called once per frame
+
+    void Start()
+    {
+        percentUnit = 1f / insanityMeter.anchorMax.x;
+        insanityPercentUnit = 100f / maxInsanityMeter;
+    }
     void Update()
     {
-        // currrentInsanityMeter = currrentInsanityMeter + CalculateInsanity();
-        // if (currentInsanityBuildUp <= 0 ) {
-        //     currrentInsanityMeter -= 0.01f;
-        // }
-        // if (currrentInsanityMeter <= 0) {
-        //     currrentInsanityMeter = 0;
-        // }
-        // Debug.Log("Insanity: " + Math.Round(currrentInsanityMeter,2));
-        // Debug.Log("Rand" + UnityEngine.Random.Range(0f, currentInsanityBuildUp));
+        currrentInsanityMeter = currrentInsanityMeter + CalculateInsanity();
+        if (currentInsanityBuildUp <= 0 ) {
+            currrentInsanityMeter -= 0.01f;
+        }
+        if (currrentInsanityMeter <= 0) {
+            currrentInsanityMeter = 0;
+        }
+        if (currrentInsanityMeter >= maxInsanityMeter) {
+            currrentInsanityMeter = maxInsanityMeter;
+        }
+
+
+
+        float currentInsanityPercent = currrentInsanityMeter * insanityPercentUnit;
+
+        insanityMeter.anchorMax = new Vector2((currentInsanityPercent * percentUnit) / 100f, insanityMeter.anchorMax.y);
+
+        Debug.Log("Insanity: " + currentInsanityPercent);
+
+
+        //Debug.Log("Insanity: " + Math.Round(currrentInsanityMeter,2));
+        //Debug.Log("Rand" + UnityEngine.Random.Range(0f, currentInsanityBuildUp));
+
     }
 
     float CalculateInsanity()
