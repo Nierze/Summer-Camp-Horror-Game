09cc870ea7be7cd1c0b02bea4b039aa9322f27c6
John Michael Coronel <139611817+kaffw@users.noreply.github.com>
2024-06-20 12:04:35 +0800

    Add
    Added Lobby Scene
    
    Selector and Highlight Initial progress
    Objects selectable with tag "Selectable"
    
    Highlight on progress

---
+++ Assets/Script/General Scripts/OutlineSelection.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class OutlineSelection : MonoBehaviour
+{
+    void Update()
+    {
+        InteractRaycast();
+    }
+
+    void InteractRaycast()
+    {
+        Vector3 cameraPosition = transform.position;
+        Vector3 forwardDirection = transform.forward;
+
+        Ray interactionRay = new Ray(cameraPosition, forwardDirection);
+        RaycastHit interactionRayHit;
+        float interactionRayLength = 50.0f;
+
+        Vector3 interactionRayEndpoint = forwardDirection * interactionRayLength;
+        UnityEngine.Debug.DrawLine(cameraPosition, interactionRayEndpoint, Color.red);
+
+        bool hitFound = Physics.Raycast(interactionRay, out interactionRayHit, interactionRayLength);
+        if (hitFound)
+        {
+            GameObject hitGameobject = interactionRayHit.transform.gameObject;
+            if (hitGameobject.CompareTag("Selectable"))
+            {
+                string hitFeedback = hitGameobject.name;
+                UnityEngine.Debug.Log(hitFeedback + "will be highlighted");
+            }
+        }
+    }
+}
