450d23c9570c185ee97aec8c67f33ab505f1e7b6
John Michael Coronel <139611817+kaffw@users.noreply.github.com>
2024-07-03 11:08:24 +0800

    Update
    Drawer Highlightable when scanned if there's an item inside

--- Assets/Script/Entity Scripts/Living Entity Scripts/Player Scripts/PlayerCamRaycast.cs
+++ Assets/Script/Entity Scripts/Living Entity Scripts/Player Scripts/PlayerCamRaycast.cs
@@ -4,6 +4,9 @@ public class PlayerCamRaycast : MonoBehaviour
 {
     public Camera mainCamera;
 
+    public MaterialControl[] highlightables;
+    private MaterialControl currentHighlight = null;
+
     void Update()
     {
         InteractRaycast();
@@ -16,5 +19,50 @@ public class PlayerCamRaycast : MonoBehaviour
         float interactionRayLength = 50.0f;
 
         Debug.DrawRay(interactionRay.origin, interactionRay.direction * interactionRayLength, Color.red);
+
+
+        if (Physics.Raycast(interactionRay, out interactionRayHit, interactionRayLength))
+        {
+            GameObject hitGameObject = interactionRayHit.transform.gameObject;
+            if (hitGameObject.CompareTag("Selectable"))
+            {
+                UnityEngine.Debug.Log(hitGameObject.name);
+
+                MaterialControl highlight = hitGameObject.GetComponent<MaterialControl>();
+                string hitFeedback = hitGameObject.name;
+
+                if (highlight != null)
+                {
+                    //Debug.Log("highlight = " + highlight);
+                    //Debug.Log("hitFeedback = " + hitFeedback);
+
+                    if (currentHighlight != highlight)
+                    {
+                        if (currentHighlight != null)
+                        {
+                            currentHighlight.DisableMaterial();
+                        }
+                        highlight.EnableMaterial();
+                        currentHighlight = highlight;
+                    }
+                }
+            }
+            else
+            {
+                if (currentHighlight != null)
+                {
+                    currentHighlight.DisableMaterial();
+                    currentHighlight = null;
+                }
+            }
+        }
+        else
+        {
+            if (currentHighlight != null)
+            {
+                currentHighlight.DisableMaterial();
+                currentHighlight = null;
+            }
+        }
     }
 }
