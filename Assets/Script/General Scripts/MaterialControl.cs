0ab46c31edf24c69316bdd2257098af9dd1654b1
John Michael Coronel <139611817+kaffw@users.noreply.github.com>
2024-07-06 04:37:08 +0800

    Update
    Lobby laptop asset updated

--- Assets/Script/General Scripts/MaterialControl.cs
+++ Assets/Script/General Scripts/MaterialControl.cs
@@ -13,69 +13,29 @@ public class MaterialControl : MonoBehaviour
 
     private bool enableDisableMaterial = false;
     
-    Material[] materials; //= meshRenderer.materials;
+    Material[] materials;
 
     void Start()
     {
-        //parentObject = transform.parent.gameObject;
-        //meshRenderer = GetComponent<MeshRenderer>();
         materials = meshRenderer.materials;
-        originalMaterial = meshRenderer.materials[1];
-        transparentMaterial = meshRenderer.materials[0];
+        if (materials.Length > 1)
+        {
+            originalMaterial = materials[materials.Length - 1];
+            transparentMaterial = materials[0];
+        }
 
         DisableMaterial();
     }
     
-    void Update()
-    {
-        
-    }
-    
     public void DisableMaterial()
     {
-        //Material[] materials = meshRenderer.materials;
-        materials[1] = transparentMaterial;
+        materials[materials.Length - 1] = transparentMaterial;
         meshRenderer.materials = materials;
     }
 
     public void EnableMaterial()
     {
-        //Material[] materials = meshRenderer.materials;
-        materials[1] = originalMaterial;
+        materials[materials.Length - 1] = originalMaterial;
         meshRenderer.materials = materials;
     }
 }
-
-/*
-    public GameObject parentObject;
-    private MeshRenderer meshRenderer;
-    public Material transparentMaterial;
-    private Material originalMaterial;
-    private bool materialEnabled = true;
-
-    void Start()
-    {
-        parentObject = transform.parent.gameObject;
-        meshRenderer = parentObject.GetComponent<MeshRenderer>();
-        originalMaterial = meshRenderer.materials[1];
-    }
-    
-    void Update()
-    {
-        DisableMaterial();
-    }
-    
-    public void DisableMaterial()
-    {
-        Material[] materials = meshRenderer.materials;
-        materials[1] = transparentMaterial;
-        meshRenderer.materials = materials;
-    }
-
-    public void EnableMaterial()
-    {
-        Material[] materials = meshRenderer.materials;
-        materials[1] = originalMaterial;
-        meshRenderer.materials = materials;
-    }
-*/
