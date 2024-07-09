b965821028b8d241cad60c55445e4351f1ab8739
John Michael Coronel <139611817+kaffw@users.noreply.github.com>
2024-07-10 00:14:46 +0800

    Update
    Scan Item and Observe Item

--- Assets/Script/Entity Scripts/Living Entity Scripts/Player Scripts/PlayerCamRaycast.cs
+++ Assets/Script/Entity Scripts/Living Entity Scripts/Player Scripts/PlayerCamRaycast.cs
@@ -1,94 +1,3 @@
-/*using UnityEngine;
-using Cinemachine;
-
-public class PlayerCamRaycast : MonoBehaviour
-{
-    public Camera mainCamera;
-    public CinemachineVirtualCamera cmVC;
-
-    private GameObject hitGameObject;
-    public bool isInspecting = false;
-    private Vector3 originalPosition;
-    private Quaternion originalRotation;
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E) && !isInspecting)
-        {
-            InteractRaycast();
-        }
-        else if (Input.GetKeyDown(KeyCode.E) && isInspecting)
-        {
-            StopInspecting();
-        }
-
-        if (isInspecting)
-        {
-            Inspecting();
-        }
-    }
-
-    void InteractRaycast()
-    {
-        Ray interactionRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit interactionRayHit;
-
-        if (Physics.Raycast(interactionRay, out interactionRayHit))
-        {
-            hitGameObject = interactionRayHit.transform.gameObject;
-            if (hitGameObject.CompareTag("Scannable"))
-            {
-                StartInspecting(hitGameObject);
-            }
-        }
-    }
-
-    void StartInspecting(GameObject obj)
-    {
-        isInspecting = true;
-        originalPosition = obj.transform.position;
-        originalRotation = obj.transform.rotation;
-
-        // Disable the virtual camera while inspecting
-        cmVC.enabled = false;
-
-        // Move the object up slightly for inspection
-        obj.transform.position += Vector3.up * 1.0f;
-    }
-
-    void StopInspecting()
-    {
-        isInspecting = false;
-
-        // Restore original position and rotation of the inspected object
-        if (hitGameObject != null)
-        {
-            hitGameObject.transform.position = originalPosition;
-            hitGameObject.transform.rotation = originalRotation;
-        }
-
-        // Re-enable the virtual camera
-        cmVC.enabled = true;
-
-        hitGameObject = null;
-    }
-
-    void Inspecting()
-    {
-        // Rotate the inspected object based on mouse input
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-
-        float rotationSpeed = 1.5f;
-
-        float rotationY = mouseX * rotationSpeed;
-        float rotationX = -mouseY * rotationSpeed;
-
-        hitGameObject.transform.Rotate(Vector3.up, rotationY, Space.World);
-        hitGameObject.transform.Rotate(Vector3.right, rotationX, Space.World);
-    }
-}*/
-
 using UnityEngine;
 using Cinemachine;
 using System.Collections;
@@ -153,8 +62,8 @@ public class PlayerCamRaycast : MonoBehaviour
             {
                 if (Input.GetKeyDown(KeyCode.E) && !isInspecting)
                 {
-                    //cmVC.enabled = false;
-                    //cmBrain.enabled = false;
+                    cmVC.enabled = false;
+                    cmBrain.enabled = false;
 
                     UnityEngine.Debug.Log("Inspect Object Function");
                     originalPosition = hitGameObject.transform.position;
