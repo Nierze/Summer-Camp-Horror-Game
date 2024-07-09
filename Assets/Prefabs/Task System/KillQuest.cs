396626d22618274ce92080849f8eccc32a51b7cd
Ren <melchorbulanon2@gmail.com>
2024-07-10 01:17:51 +0800

    yes

---
+++ Assets/Prefabs/Task System/KillQuest.cs
@@ -0,0 +1,15 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class KillQuest : ITaskInterface
+// {
+//     public int killCount = 0;
+//     public int killGoal = 0;
+//     public string target;
+
+//     public void questRequirement()
+//     {
+//         Debug.Log($"Kill {killCount}/{killGoal} {target}.");
+//     }
+// }
